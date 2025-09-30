88241c475f6afa5a5e11893381556330c0e68479
Bridger Fiore <bf253172@umconnect.umt.edu>
2025-09-30 17:49:47 -0600

    Initial project setup with .gitignore and requirements.txt

---
+++ pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+def home_page_view(request):
+    context = {
+        "inventory_list": ["Widget 1", "Widget 2", "Widget 3"],
+        "greeting": "Thank you for visiting!",
+    }
+    return render(request, "home.html", context)  # Updated to match the new template path
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "1234 Main St, Anytown, USA"
+        context["phone_number"] = "(123) 456-7890"
+        return context
